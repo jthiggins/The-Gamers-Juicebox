2897299b3c6dfd1ca6c336bd43efdc467863cf39
the-david-wang <62066842+the-david-wang@users.noreply.github.com>
2020-04-01 01:01:01 -0400

    Update browse.svelte

--- src/routes/browse.svelte
+++ src/routes/browse.svelte
@@ -12,6 +12,40 @@
         const { top } = container.getBoundingClientRect();
         container.style.height = `calc(98vh - ${top}px)`;
     });
+	
+function drop() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function sortGames(by) {
+	if (by == "A"){
+		games.title.sort();
+	}
+	if (by == "Z"){
+		games.title.sort();
+		games.title.reverse()
+	}
+	if (by == "A"){
+		games.price.sort(function(a, b){return a - b});
+	}
+	if (by == "A"){
+		games.price.sort(function(a, b){return b - a});
+	}
+}
+
 </script>
 
 <style>
@@ -24,6 +58,17 @@
 </style>
 
 <h1 class="center">Browse Games</h1>
+
+<div class="dropdown">
+  <button onclick="drop()" class="dropbtn">Dropdown</button>
+  <div id="myDropdown" class="dropdown-content">
+    <a onclick="sortGames(A)">A-Z</a>
+	<a onclick="sortGames(Z)">Z-A</a>
+	<a onclick="sortGames($)">$-$$$</a>
+	<a onclick="sortGames($$$)">$$$-$</a>
+  </div>
+</div>
+
 <div bind:this={container}>
     {#each games as game}
     <GameDisplay {game} style="margin-right: 10px; margin-bottom: 10px;"/>
