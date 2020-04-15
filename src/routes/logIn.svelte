513a89a7cc42161e80d41e61a4d0fa4b6c748428
prentoam <44214493+prentoam@users.noreply.github.com>
2020-04-15 19:17:25 -0400

    Create logIn.svelte

---
+++ src/routes/logIn.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+    <title>Browse Games - The Gamer's Juicebox</title>
+</svelte:head>
+
+<script>
+    import { onMount } from 'svelte';
+    onMount(() => {
+        const { top } = container.getBoundingClientRect();
+        container.style.height = `calc(98vh - ${top}px)`;
+    });
+</script>
+
+<h1 class="center">Log In To Your Account</h1>
+<div style="border: solid black 1px; margin:20px; padding:20px">
+<form>
+
+<div style="margin-bottom: 10px">
+<label for="uName">User Name</label>
+<input type="text" required pattern="([A-Za-z0-9]+)\s*" id="uName" name="uName">
+</div>
+
+<div style="margin-bottom: 10px">
+<label for="pass">Password</label>
+<input type="password" required id="pass" name="pass">
+</div>
+
+<div style="margin-bottom: 10px">
+<input type="login" value="Log In">
+</div>
+</form>
+</div>
