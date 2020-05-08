111609799c6cb083eb85ab9bd4cf96356d7e1222
prentoam <44214493+prentoam@users.noreply.github.com>
2020-05-08 13:00:10 -0400

    Update viewRequests.json.js

--- src/routes/viewRequests.json.js
+++ src/routes/viewRequests.json.js
@@ -4,6 +4,7 @@ export async function get(req, res, next) {
     try {
         const requests = await getRequests();
         res.json(requests.recordset);
+        console.log(requests);
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
