a02e9b01dc3b915e9cd0d3960ab081b16b66e809
prentoam <44214493+prentoam@users.noreply.github.com>
2020-05-08 12:40:29 -0400

    Update viewRequests.json.js

--- src/routes/viewRequests.json.js
+++ src/routes/viewRequests.json.js
@@ -3,7 +3,7 @@ import { getRequests } from "./_dbApi";
 export async function get(req, res, next) {
     try {
         const requests = await getRequests();
-        res.json(requests);
+        res.json(requests.recordset);
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
